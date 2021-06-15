2a30c0a2f36f19308e18a40fa4f46f84d1f0e6a3
Yash Raj Singh <yashrajsingh807@gmail.com>
2021-06-15 21:54:08 +0530

    Got to know about Jobs, waiting, cancellation, Async, Await, LifeScopes and ViewModelScopes.

--- app/src/main/java/com/example/coroutines/MainActivity.kt
+++ app/src/main/java/com/example/coroutines/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.coroutines
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.lifecycle.lifecycleScope
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.*
+import kotlin.system.measureTimeMillis
 
 class MainActivity : AppCompatActivity() {
     val TAG = "MainAct"
@@ -59,30 +62,137 @@ class MainActivity : AppCompatActivity() {
         // which also blocks our main thread
         // We can directly write a new launch{} inside runBlock without GlobalScope
 
-        Log.d(TAG, "BEFORE RUNBLOCK")
-        runBlocking {
+//        Log.d(TAG, "BEFORE RUNBLOCK")
+//        runBlocking {
+//
+//            launch {
+//                delay(3000L)
+//                Log.d(TAG, "IO Coroutine 1")
+//            }
+//            launch {
+//                delay(3000L)
+//                Log.d(TAG, "IO Coroutine 2")
+//            }
+//
+//            Log.d(TAG, "Started RUNBLOCK")
+//            delay(7000L)
+//            Log.d(TAG, "Ending RUNBLOCK")
+//        }
+//        Log.d(TAG, "AFTER RUNBLOCK")
 
-            launch {
-                delay(3000L)
-                Log.d(TAG, "IO Coroutine 1")
-            }
-            launch {
-                delay(3000L)
-                Log.d(TAG, "IO Coroutine 2")
+
+        // The GlobalScope.launch returns a job, So we can store them in a variable
+        // Then we can wait for that job to finish by writing JOBNAME.join()
+        // But this is a suspend function so either call it in a
+        // GlobalScope.launch or in a runBlock
+
+        //val job = GlobalScope.launch(Dispatchers.Default) {
+//            repeat(3)
+//            {
+//                Log.d(TAG, "Coroutine is still working!")
+//                delay(3000L)
+//            }
+        //NOW THIS WILL NOT GET CANCEL BY CANCEL FUNCTION AUTOMATICALLY
+        //BECAUSE IT WAS BUSY IN CALCULATIONS AND LOOP THAT IT DIDN'T NOTICED IT
+        //SO WE NEED TO MANUALLY CHECK IT BY USING isActive FUNCTION
+        //withTimeout becomes useful if we need to cancel a function/program
+        //if it take more time then assigned in withTimeout function
+
+//            Log.d(TAG, "Long Calculations Starting!")
+//            withTimeout(1500L)
+//            {
+//                for(i in 30..40)
+//                {
+//                    if(isActive) {
+//                        Log.d(TAG, "For $i the value is : ${fibo(i)}")
+//                    }
+//                }
+//            }
+//
+//            Log.d(TAG, "Long Calculations Ended!")
+//        }
+
+//        runBlocking {
+//            // here we waited for our coroutine to get finished
+////            job.join()
+//            delay(5000L)
+//            job.cancel()
+//            Log.d(TAG,"Canceled The Job!")
+//        }
+        //}
+
+        // If we have more than one suspend function in our coroutine,
+        //they will be executed sequentially in order
+
+//        GlobalScope.launch(Dispatchers.IO) {
+//            //Showing how time delay of both functions would add up
+//            // Cool function to measure time
+//            val time = measureTimeMillis {
+//
+//                // To escape from the execution hell of one after another and
+//                // adding up the time, we have Async Function
+//                // It is similar to launch function, means it create a new thread
+//                // to perform tasks parallel to each other, but it is very fast
+//                // So we use .await() to let the task finish up completely.
+//
+//                val q1 = async { callingSuspend() }
+//                val q2 = async { callingSuspend2() }
+//                Log.d(TAG, "${q1.await()}")
+//                Log.d(TAG, "${q2.await()}")
+//            }
+//            Log.d(TAG, "Time in total would be : $time ms")
+//        }
+
+        // Sometimes we don't want to use GlobalScope as we don't want
+        // our coroutine to have a long life till the existence of application
+
+        btnSecond.setOnClickListener{
+            // Here even after the destruction of our main activity
+            // the while loop will keep running because it is in Global Scope
+            // and due to that, it is running in a new thread on parallel
+            // it may cause memory leaks, So to stop that we can replace
+            // GlobalScope with lifecycle Scope, whose scope it defined
+            // till the life of that particular activity
+
+//            GlobalScope.launch {
+//                while(true)
+//                {
+//                    delay(1000L)
+//                    Log.d(TAG, "Yeah, it's still up!")
+//                }
+//            }
+
+            lifecycleScope.launch {
+                while(true)
+                {
+                    delay(1000L)
+                    Log.d(TAG, "Yeah, it's still up!")
+                }
             }
 
-            Log.d(TAG, "Started RUNBLOCK")
-            delay(7000L)
-            Log.d(TAG, "Ending RUNBLOCK")
+            GlobalScope.launch {
+                delay(5000L)
+                Log.d(TAG, " Starting 2nd Activity ")
+                Intent(this@MainActivity, Second_Activity::class.java).also {
+                    startActivity(it)
+                    finish()
+                }
+            }
         }
-        Log.d(TAG, "AFTER RUNBLOCK")
+
+
+    }
+    private fun fibo(n: Int): Long{
+        return if(n == 0) 0
+        else if(n == 1) 1
+        else fibo(n-1) + fibo(n-2)
     }
     suspend fun callingSuspend(): String{
         delay(2000L)
         return "Returned from a suspend function!"
     }
     suspend fun callingSuspend2(): String{
-        delay(7000L)
+        delay(2000L)
         return "Returned from a suspend 2 function!"
     }
 }
